0fe5cfe15c67867c18f58c21123348c5f550b3c5
carlos <robertorsilva.krlos@gmail.com>
2025-11-17 22:49:51 -0300

    first commit

--- src/app/(auth)/exp/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Expedicao } from "@/_modules/expedicao_/views";
-
-export default function ExpedicaoPage() {
-  return (
-    <div className="p-4">
-      <Expedicao />
-    </div>
-  );
-}
