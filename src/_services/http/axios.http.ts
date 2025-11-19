e5a4504c4ce933571b5a801a9448ee5d26e7b3ed
carlos <robertorsilva.krlos@gmail.com>
2025-11-18 21:34:15 -0300

    ajuste endpoint

--- src/_services/http/axios.http.ts
+++ src/_services/http/axios.http.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig, AxiosError } from 'axios';
 import { getSession } from 'next-auth/react';
 
 export const AXIOS_INSTANCE = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
